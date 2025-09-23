1f90bf74d63c7bef8395deffc4104cc1dd244a6f
jay <lyl.jay@qq.com>
2025-09-23 16:51:44 +0800

    UPDATE 用户登录功能 不可拦截登录/验证页面，修正拦截器白名单完整路径

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -7,17 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 注册添加拦截器
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
+                //排除不需要拦截的目录
                 .excludePathPatterns(
                         "/shop/**",
                         "/voucher/**",
                         "/shop-type/**",
                         "/upload/**",
                         "/blog/hot",
-                        "user/code",
-                        "user/login"
+                        "/user/code",
+                        "/user/login"
                 );
     }
 }
--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -7,17 +7,22 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+//请求响应拦截器
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //获取Session
         HttpSession session = request.getSession();
+        //获取Session中的用户
         Object user = session.getAttribute("user");
+        //判断用户是否存在
         if (user == null) {
+            //不存在则拦截，反正401
             response.setStatus(401);
             return false;
         }
+        //用户存在则保持到ThreadLocal
         UserHolder.saveUser((User)user);
         return true;
     }
