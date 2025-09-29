be68632df70aacbe5bc445ea3073ab1fb6f04470
jay <lyl.jay@qq.com>
2025-09-29 11:33:14 +0800

    UPDATE 发布笔记时推送给所有粉丝

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,7 +1,8 @@
 package com.hmdp.service;
 
-import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Blog;
 
 /**
  * <p>
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    Result saveBolg(Blog blog);
 }
--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IFollowService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +25,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    private final IFollowService followService;
+    private final StringRedisTemplate redisTemplate;
+
+    public BlogServiceImpl(IFollowService followService, StringRedisTemplate redisTemplate) {
+        this.followService = followService;
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public Result saveBolg(Blog blog) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店博文笔记
+        boolean success = save(blog);
+        if (!success){
+            return Result.fail("新建笔记失败");
+        }
+        //查询所有粉丝
+        List<Follow> follows = followService.query().eq("follower_id", blog.getUserId()).list();
+        //将发布的笔记推送给粉丝
+        for (Follow follow : follows) {
+            Long id = follow.getId();
+            String key = "feed:"+id;
+            redisTemplate.opsForZSet().add(key,blog.getId().toString(),System.currentTimeMillis());
+        }
+        // 返回id
+        return Result.ok(blog.getId());
+    }
 }
